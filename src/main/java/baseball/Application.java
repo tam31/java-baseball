b575a99cf13ac339a5b3d5d0c62c241beda5c02b
tam31 <wxogud3377@naver.com>
2023-09-18 20:28:48 +0900

    Feat: 사용자 숫자입력 사용자 숫자입력 완료 Docs: 사용자 숫장입력 완료 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,5 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Output output = new Output();
         output.printStartGame();
+        Input input = new Input();
+        input.enterGameNumber();
     }
 }
---
+++ src/main/java/baseball/Input.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+public class Input {
+
+    public void enterGameNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String number = Console.readLine();
+    }
+}
