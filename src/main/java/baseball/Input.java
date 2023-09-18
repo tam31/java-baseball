a4af047873bc572538dc7c627bce86cc9c594aec
tam31 <wxogud3377@naver.com>
2023-09-18 21:28:44 +0900

    Feat: 사용자가 입력한 값 저장하기 사용자의 데이터 저장기능 구현 Docs: 사용자데이터 저장 추가 Docs: 사용자데이터 저장 완료 체크

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -2,9 +2,10 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 public class Input {
-
+    User user = new User();
     public void enterGameNumber(){
         System.out.print("숫자를 입력해주세요 : ");
         String number = Console.readLine();
+        user.addInputNumber(number);
     }
 }
---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,15 @@
+package baseball;
+
+public class User {
+
+    private String inputNumber;
+
+    public String getInputNumber() {
+        return inputNumber;
+    }
+
+    public void addInputNumber(String inputNumber) {
+        this.inputNumber = inputNumber;
+    }
+
+}
