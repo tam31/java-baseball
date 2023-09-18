ec5068a2ec81f9a777c1f105f6f7e8dafe7bff85
tam31 <wxogud3377@naver.com>
2023-09-19 01:38:28 +0900

    Feat: 서로다른 숫자인지 검사 기능 데이터 유효성 검사 -> 3. 데이터의 중복검사  기능 완료 Docs: 데이터의 중복 검사완료  체크 및 데이터 유효성 검사 완료 체크

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -5,18 +5,13 @@ public class Exception {
     public void checkException(String number){
         checkNumber(number);
         isSizeTree(number);
-    }
-    private void isSizeTree(String number){
-        if(number.length() != 3){
-            throw new IllegalArgumentException("입력하신 숫자가 3가지가 아닙니다. 잘못된 값으로 프로그램을 종료합니다.");
-        }
+        isNotDuplicate(number);
     }
     private void checkNumber(String number) {
         if(isNotNumber(number)){
             throw new IllegalArgumentException(number +"는 숫자가 아닙니다. 숫자를 입력하시오. 잘못된 값으로 프로그램이 종료됩니다.");
         }
     }
-
     private boolean isNotNumber(String number){
         for(int i=0; i<number.length(); i++){
             if(Character.isAlphabetic(number.charAt(i))){
@@ -24,5 +19,19 @@ public class Exception {
             }
         }
         return false;
+    }
+
+    private void isSizeTree(String number){
+        if(number.length() != 3){
+            throw new IllegalArgumentException("입력하신 숫자가 3가지가 아닙니다. 잘못된 값으로 프로그램을 종료합니다.");
+        }
+    }
+
+    private void isNotDuplicate(String number){
+        if(number.charAt(0) == number.charAt(1) ||
+                number.charAt(1) == number.charAt(2) ||
+                number.charAt(0) == number.charAt(2)){
+            throw new IllegalArgumentException(number+"중 중복되는 값이 있습니다. 잘못된 값으로 프로그램을 종료합니다.");
+        }
     }
 }
