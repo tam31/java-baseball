2ce2ad91f48353a880119b2f37a7858d340ab645
tam31 <wxogud3377@naver.com>
2023-09-19 01:31:04 +0900

    Feat: 3가지의 숫자인지 검사 기능 데이터 유효성 검사 -> 2. 데이터의 크기가 3인지 검사 기능 완료 Docs: 데이터의 크기 검사 완료 체크

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -4,8 +4,13 @@ public class Exception {
 
     public void checkException(String number){
         checkNumber(number);
+        isSizeTree(number);
     }
-
+    private void isSizeTree(String number){
+        if(number.length() != 3){
+            throw new IllegalArgumentException("입력하신 숫자가 3가지가 아닙니다. 잘못된 값으로 프로그램을 종료합니다.");
+        }
+    }
     private void checkNumber(String number) {
         if(isNotNumber(number)){
             throw new IllegalArgumentException(number +"는 숫자가 아닙니다. 숫자를 입력하시오. 잘못된 값으로 프로그램이 종료됩니다.");
