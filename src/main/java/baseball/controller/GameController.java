2c9acc0ebea877bab3377eb1678fb24ffc8b63bf
tam31 <wxogud3377@naver.com>
2023-09-19 17:14:35 +0900

    Fix: Input의 코드 수정 및 출력문 구현 Docs: 숫자 입력 메시지 완료 체크

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -2,16 +2,19 @@ package baseball.controller;
 
 import baseball.RandomNumber;
 import baseball.model.RandomGameNumber;
+import baseball.view.Input;
 import baseball.view.Output;
 
 public class GameController {
 
     Output output = new Output();
+    Input input = new Input();
     RandomNumber randomNumber = new RandomNumber();
     RandomGameNumber computer = new RandomGameNumber();
     public void proceedGame() {
         output.printStartGame();
         computer.addComputerNumber(randomNumber.selectRandomNumber());
+        input.enterGameNumber();
         output.printEndGame();
     }
 }
--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -1,15 +1,8 @@
 package baseball.view;
 
-import baseball.exception.DataException;
-import baseball.model.UserGameNumber;
-import camp.nextstep.edu.missionutils.Console;
 public class Input {
-    UserGameNumber userGameNumber = new UserGameNumber();
-    DataException dataException = new DataException();
+
     public void enterGameNumber(){
         System.out.print("숫자를 입력해주세요 : ");
-        String number = Console.readLine();
-        dataException.checkException(number);
-        userGameNumber.addUserNumber(number);
     }
 }
