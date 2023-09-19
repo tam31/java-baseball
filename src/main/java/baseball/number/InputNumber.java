cec19ff8238e73325fd299baebe27f646d29f60f
tam31 <wxogud3377@naver.com>
2023-09-20 03:07:44 +0900

    Refactor: 입력받은  데이터를 전체적으로 검사할 수 있도록 수정

--- src/main/java/baseball/number/InputNumber.java
+++ src/main/java/baseball/number/InputNumber.java
@@ -4,9 +4,9 @@ import baseball.exception.DataException;
 import camp.nextstep.edu.missionutils.Console;
 public class InputNumber {
 
+    DataException exception = new DataException();
     public String inputUserNumber(){
         String inputUser =  Console.readLine();
-        DataException exception = new DataException();
         exception.checkException(inputUser);
         return inputUser;
     }
