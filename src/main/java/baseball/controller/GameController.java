631aa5e3b94a68b2d01244f26d99f50c2b46f986
tam31 <wxogud3377@naver.com>
2023-09-19 17:02:03 +0900

    Feat: RandomNumber클래스의 서로다른 3개의 숫자 구현 기능 Add: RandomNumber클래스 생성(중복하지 않는 숫자를 뽑기 위해) Docs: RandomNumber의 리스트 만들기 완료 체크

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,13 +1,15 @@
 package baseball.controller;
 
+import baseball.RandomNumber;
 import baseball.view.Output;
 
 public class GameController {
 
     Output output = new Output();
+    RandomNumber randomNumber = new RandomNumber();
     public void proceedGame() {
         output.printStartGame();
-
+        System.out.println(randomNumber.selectRandomNumber());
         output.printEndGame();
     }
 }
