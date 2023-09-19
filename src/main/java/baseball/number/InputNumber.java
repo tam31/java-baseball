49d6a49d9c58dbcb55465c488f4ceec52bb9cb8b
tam31 <wxogud3377@naver.com>
2023-09-19 17:39:43 +0900

    Feat: 입력받은 데이터 유효성 검사 구현 Docs: 데이터 유효성검사 완료 체크

--- src/main/java/baseball/number/InputNumber.java
+++ src/main/java/baseball/number/InputNumber.java
@@ -1,9 +1,13 @@
 package baseball.number;
 
+import baseball.exception.DataException;
 import camp.nextstep.edu.missionutils.Console;
 public class InputNumber {
 
     public String inputUserNumber(){
-        return Console.readLine();
+        String inputUser =  Console.readLine();
+        DataException exception = new DataException();
+        exception.checkException(inputUser);
+        return inputUser;
     }
 }
