da36af614f82abfca2b3be7ac0421f87e59bebdf
tam31 <wxogud3377@naver.com>
2023-09-19 16:43:57 +0900

    Feat: 게임진행 controller 구현 Add: controller 패키지와 GameController클래스 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,11 @@
 package baseball;
 
-import baseball.model.RandomGameNumber;
-import baseball.view.Input;
-import baseball.view.Output;
+
+import baseball.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        Output output = new Output();
-        output.printStartGame();
-        RandomGameNumber randomGameNumber = new RandomGameNumber();
-        Input input = new Input();
-        input.enterGameNumber();
-        System.out.println(randomGameNumber.getComputerNumber());
-        System.out.println(input.userGameNumber.getUserNumber());
-        output.printEndGame();
+        GameController gameController = new GameController();
+        gameController.proceedGame();
     }
 }
---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,10 @@
+package baseball.controller;
+
+import baseball.view.Output;
+
+public class GameController {
+
+    public void proceedGame() {
+        System.out.println("게임 시작");
+    }
+}
