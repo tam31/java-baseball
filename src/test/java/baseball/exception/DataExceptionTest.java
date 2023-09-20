8b6687ec7a437e423de8c3779344176e7eb74e8a
tam31 <wxogud3377@naver.com>
2023-09-20 22:22:20 +0900

    Test: 입력받은 데이터가 중복이라면 예외발생테스트

--- src/test/java/baseball/exception/DataExceptionTest.java
+++ src/test/java/baseball/exception/DataExceptionTest.java
@@ -25,4 +25,13 @@ class DataExceptionTest {
         //then 크기가 3이 아니므로 예외발생
         assertThrows(IllegalArgumentException.class, ()-> exception.isSizeTree(input));
     }
+
+    @Test
+    public void 중복검사() throws Exception{
+        //given 이런게 주어졋을때
+        String input = "333";
+        //when 이걸 실행하면
+        //then 결과가 중복이 발생함으로 예외처리
+        assertThrows(IllegalArgumentException.class, ()-> exception.isNotDuplicate(input));
+    }
 }
