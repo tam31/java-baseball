c4f4e91d1026679cb1f50bb30e258a1b959f8114
tam31 <wxogud3377@naver.com>
2023-09-20 22:13:30 +0900

    Style: 데이터 유효성 검사 메소드 접근제한 변경

--- src/main/java/baseball/exception/DataException.java
+++ src/main/java/baseball/exception/DataException.java
@@ -7,12 +7,12 @@ public class DataException {
         isSizeTree(number);
         isNotDuplicate(number);
     }
-    private void checkNumber(String number) {
+    public void checkNumber(String number) {
         if(isNotNumber(number)){
             throw new IllegalArgumentException(number +"는 숫자가 아닙니다. 숫자를 입력하시오. 잘못된 값으로 프로그램이 종료됩니다.");
         }
     }
-    private boolean isNotNumber(String number){
+    public boolean isNotNumber(String number){
         for(int i=0; i<number.length(); i++){
             if(Character.isAlphabetic(number.charAt(i))){
                 return true;
@@ -21,13 +21,13 @@ public class DataException {
         return false;
     }
 
-    private void isSizeTree(String number){
+    public void isSizeTree(String number){
         if(number.length() != 3){
             throw new IllegalArgumentException("입력하신 숫자가 3가지가 아닙니다. 잘못된 값으로 프로그램을 종료합니다.");
         }
     }
 
-    private void isNotDuplicate(String number){
+    public void isNotDuplicate(String number){
         if(number.charAt(0) == number.charAt(1) ||
                 number.charAt(1) == number.charAt(2) ||
                 number.charAt(0) == number.charAt(2)){
