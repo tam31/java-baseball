ece59341469abe3f0c7fe3dedff7fb646ae43f15
tam31 <wxogud3377@naver.com>
2023-09-21 01:28:50 +0900

    Refactor: 상수메세지 초기화 및 메소드안의 메세지 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,12 +1,14 @@
 package baseball.view;
 
 public class InputView {
+    private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String INPUT_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void enterGameNumber(){
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_NUMBER);
     }
 
     public void isRestartGame() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(INPUT_RESTART);
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,32 +2,39 @@ package baseball.view;
 
 public class OutputView {
 
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String THREE_STRIKE_MESSAGE = "3스트라이크";
+    private static final String ZERO_STRIKE_BALL_MESSAGE = "낫싱";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+
     public void printStartGame(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(START_MESSAGE);
     }
 
     public void printEndGame(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(END_MESSAGE);
     }
 
 
     public void ThreeStrike() {
-        System.out.println("3스트라이크");
+        System.out.println(THREE_STRIKE_MESSAGE);
     }
 
     public void isZeroStrikeAndBall() {
-        System.out.println("낫싱");
+        System.out.println(ZERO_STRIKE_BALL_MESSAGE);
     }
 
     public void isZeroStrike(int ball) {
-        System.out.println(ball+"볼");
+        System.out.println(ball+BALL_MESSAGE);
     }
 
     public void isZeroBall(int strike) {
-        System.out.println(strike+"스트라이크");
+        System.out.print(strike+STRIKE_MESSAGE);
     }
 
     public void isStrikeAndBall(int strike, int ball) {
-        System.out.println(ball+"볼 "+strike+"스트라이크");
+        System.out.println(ball+BALL_MESSAGE+" "+strike+STRIKE_MESSAGE);
     }
 }
