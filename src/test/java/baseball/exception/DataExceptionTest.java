dbc9658c153a24dfff4aaad18d3459c2b5951d32
tam31 <wxogud3377@naver.com>
2023-09-20 22:11:03 +0900

    Add: 데이터유효성 검사 테스트 파일 생성

---
+++ src/test/java/baseball/exception/DataExceptionTest.java
@@ -0,0 +1,19 @@
+package baseball.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataExceptionTest {
+
+    @Test
+    public void test() throws Exception{
+        //given 이런게 주어졋을때
+
+        //when 이걸 실행하면
+
+        //then 결과가 이렇게 된다
+
+    }
+
+}
